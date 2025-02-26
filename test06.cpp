5175c444ae76a4d05018ddfd8969fb70407e6af6
WAsD-Ddos <mitja3375@gmail.com>
2025-02-26 18:52:17 +0300

    Full

--- test06.cpp
+++ test06.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-void init(int* array, int size) {
-
-}
-
-int* test(int* size) {
-	*size = 5;
-	int* array = new int[5];
-	*(array) = 1;
-	*(array + 1) = 2;
-	*(array + 2) = 3;
-	*(array + 3) = 4;
-	*(array + 4) = 5;
-	return array;
-}
-
-int main() {
-	int size = 0;
-	int* array = test(&size);
-
-
-	for (int i = 0; i < 5; i++)
-	{
-		cout << array[i] << " ";
-	}
-
-	delete[] array;
-
-
-	return 0;
-}   
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//void init(int* array, int size) {
+//
+//}
+//
+//int* test(int* size) {
+//	*size = 5;
+//	int* array = new int[5];
+//	*(array) = 1;
+//	*(array + 1) = 2;
+//	*(array + 2) = 3;
+//	*(array + 3) = 4;
+//	*(array + 4) = 5;
+//	return array;
+//}
+//
+//int main() {
+//	int size = 0;
+//	int* array = test(&size);
+//
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cout << array[i] << " ";
+//	}
+//
+//	delete[] array;
+//
+//
+//	return 0;
+//}   
---
+++ test07.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+	int size;
+	cout << "input size of array: " << endl;
+	cin >> size;
+
+	int* array = new int[size];
+
+	*(array) = 1;
+	*(array+1) = 11;
+	*(array+2) = 111;
+	*(array+3) = 1111;
+	*(array+4) = 11111;
+
+	    cout << *(array)     << " -  " << array[0]<< endl;
+		cout << *(array + 1) << " -  " << array[1] << endl;
+		cout << *(array + 2) << " -  " << array[2] << endl;
+		cout << *(array + 3) << " -  " << array[3] << endl;
+		cout << *(array + 4) << " -  " << array[4] << endl;
+
+
+
+	delete[] array;
+
+
+	return 0;
+}   
